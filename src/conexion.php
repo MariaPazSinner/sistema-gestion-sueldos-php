f2bc5643ba4244b8cbb405bfa51f76e7566b2fc1
Paz Sinner <mariapazsinner@gmail.com>
2026-09-03 15:56:40 -0300

    Ocultar detalles de errores de conexión

--- src/conexion.php
+++ src/conexion.php
@@ -6,13 +6,14 @@ $user = getenv('DB_USER') ?: 'root';
 $password = getenv('DB_PASSWORD') ?: '';
 $bd = getenv('DB_NAME') ?: 'basededatosproyecto';
 
+mysqli_report(MYSQLI_REPORT_OFF);
 $mysqli = mysqli_init();
 
 if (getenv('DB_SSL') === 'true') {
     mysqli_ssl_set($mysqli, null, null, null, null, null);
 }
 
-if (!mysqli_real_connect($mysqli, $host, $user, $password, $bd, $port)) {
+if (!@mysqli_real_connect($mysqli, $host, $user, $password, $bd, $port)) {
     http_response_code(500);
     exit('No se pudo conectar a la base de datos.');
 }
